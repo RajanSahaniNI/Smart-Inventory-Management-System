1d3360fdd59fee1dad3dfd331575f203bc406149
RajanSahaniNI <sahani7887@gmail.com>
2026-04-18 23:46:36 +0530

    Remove all files from repo

--- SmartInMS/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    # Based on a previous conversation where 'rajan123@' was used as the MySQL password
-    MYSQL_PASSWORD = 'rajan123@' 
-    MYSQL_DB = 'inventory_db'
